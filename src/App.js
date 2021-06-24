171caa5b61e957fbd346f2f0bccc028e6ff26610
Thomas Hurrie <thomas.hurrie@gmail.com>
2021-06-24 14:41:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FiltersList from "./Filters/index";
+//TODO: import the data model for your list of filters
+import { filterData } from "./data.js";
+
+const App = () => (
+  <div className="App">
+    {/* TODO: pass your data model as a prop to your FiltersList component */}
+    <FiltersList filters={filterData} />
+  </div>
+);
+
+export default App;
